0007aaf5206766d6c3391eb7f1478e352990aa09
Peter <schmidt.multimedia@gmail.com>
2023-04-06 10:26:16 +0200

    Vornbereitung / Arrays

--- script_01.js
+++ script_01.js
@@ -7,8 +7,19 @@
  * 
  */
 
+/*** 02. Theorie: Array */
+let arr;
+// arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,7,11,4];
+arr = [true,false,true];
+arr = ["Ich","bin","Max","Mütze"];
 
-
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Pos. im Array)
+output(arr[3]) // letze Pos. / konkret
+output(arr[arr.length-1]); // letze Pos. / allgemein
 
 /**
  *   PRAXIS
@@ -20,19 +31,20 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;
 }
-
 
 
 // Modul: Ausgabe in Konsole : Test
